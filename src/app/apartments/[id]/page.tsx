6457d21f023cf9af62d217baaeebb46203b45b99
Amir Adilov <amiradilov202@gmail.com>
2024-07-19 01:30:03 +0500

    imagine

--- src/app/apartments/[id]/page.tsx
+++ src/app/apartments/[id]/page.tsx
@@ -1,23 +1,23 @@
-"use client"
-interface Apartment {
-    id: number;
-    link: string;
-    price: string;
-    location: string;
-    floor: string;
-    number: string;
-    photos: string[];
-    characteristics: { [key: string]: string };
-    description: string;
-    site: string;
-    type: string;
-}
-
+"use client";
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { LocateIcon, TagIcon, ExternalLinkIcon } from 'lucide-react';
+import { ExternalLinkIcon } from 'lucide-react';
 import { Header } from '@/components/layout/header';
 import { Footer } from '@/components/layout/footer';
+
+interface Apartment {
+  id: number;
+  link: string;
+  price: string;
+  location: string;
+  floor: string;
+  number: string;
+  photos: string[];
+  characteristics: { [key: string]: string };
+  description: string;
+  site: string;
+  type: string;
+}
 
 const ApartmentDetails = ({ params: { id } }: { params: { id: number } }) => {
   const [apartment, setApartment] = useState<Apartment | null>(null);
@@ -71,20 +71,24 @@ const ApartmentDetails = ({ params: { id } }: { params: { id: number } }) => {
         <section className="container mx-auto grid grid-cols-1 lg:grid-cols-3 gap-6 py-12">
           <div className="lg:col-span-2">
             <div className="grid gap-4">
-            <img
-                src={selectedPhoto || ''}
-                alt="Apartment"
-                className="rounded-xl object-cover w-full md:w-1/2 h-84 mx-auto"
-            />
-              <div className="grid grid-cols-10 gap-2">
+              <div className="relative overflow-hidden rounded-lg h-[500px] w-full md:h-[600px] md:w-[800px]">
+                <img
+                  src={selectedPhoto || ''}
+                  alt="Apartment"
+                  className="object-cover w-full h-full"
+                  style={{ objectPosition: 'center' }}
+                />
+              </div>
+              <div className="flex overflow-x-auto py-2 gap-2">
                 {apartment.photos.map((photo, index) => (
-                  <img
-                    key={index}
-                    src={photo}
-                    alt="Apartment"
-                    className={`rounded-full object-cover w-full h-20 cursor-pointer ${selectedPhoto === photo ? 'border-4 border-blue-600' : ''}`}
-                    onClick={() => setSelectedPhoto(photo)}
-                  />
+                  <div key={index} className="flex-none">
+                    <img
+                      src={photo}
+                      alt="Apartment"
+                      className={`w-20 h-20 rounded-md cursor-pointer object-cover ${selectedPhoto === photo ? 'border-4 border-blue-600' : ''}`}
+                      onClick={() => setSelectedPhoto(photo)}
+                    />
+                  </div>
                 ))}
               </div>
             </div>
@@ -96,9 +100,6 @@ const ApartmentDetails = ({ params: { id } }: { params: { id: number } }) => {
               <div className="flex items-center gap-2">
                 <span>{apartment.location}</span>
               </div>
-              {/* <div className="flex items-center gap-2 text-gray-600">
-                <span className="text-sm">{apartment.floor}</span>
-              </div> */}
               <div className="grid gap-2">
                 {Object.entries(characteristics).map(([key, value], index) => (
                   <div key={index} className="flex justify-between text-gray-600 gap-24">
