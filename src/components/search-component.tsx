9068835600f9cc46fc3037a0e217e3a337606888
Amir Adilov <amiradilov202@gmail.com>
2024-07-18 23:34:22 +0500

    4/3

--- src/components/search-component.tsx
+++ src/components/search-component.tsx
@@ -314,11 +314,15 @@ useEffect(() => {
             
             <Card key={apartment.link} className="w-full border-[#CFCFCF] border-0 border-b-[0.5px] mx-auto grid grid-cols-1 md:grid-cols-[1fr_1.5fr] gap-6 p-6 sm:p-8 md:p-10 rounded-xl"
             style={{ marginLeft: '0', paddingLeft: '0' }}>
-              <div className="relative overflow-hidden rounded-lg h-[150px] w-full md:h-[200px] md:w-[400px]">
+              <div className="relative overflow-hidden rounded-lg w-full md:w-[400px] aspect-[4/3]">
                 <Carousel showArrows={true} showThumbs={false}>
                   {apartment.photos.map((photo, index) => (
-                    <div key={index}>
-                      <img src={photo} alt={`Property Image ${index + 1}`} className="w-full h-full object-cover object-center" style={{ objectPosition: 'center' }} />
+                    <div key={index} className="h-full">
+                      <img 
+                        src={photo} 
+                        alt={`Property Image ${index + 1}`} 
+                        className="w-full h-full object-cover"
+                      />
                     </div>
                   ))}
                 </Carousel>
